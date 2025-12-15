8a8de8009fbcdc7678e3dcfe87d828bcb4f6bbc9
Joon Kim <joonkim@Joons-MBP.attlocal.net>
2025-12-14 20:34:32 -0800

    push recent changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/snaphoa-account-creation/',
   plugins: [react()],
+  base: '/snaphoa-account-creation/', // <-- important!
 });
