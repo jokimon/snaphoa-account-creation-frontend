656153645d35048ad3ae9b2925d196ce293f8dcc
Joon Kim <joonkim@Joons-MBP.attlocal.net>
2025-12-14 20:59:32 -0800

    fix nested router issue in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // <-- HashRouter
+import { Routes, Route } from 'react-router-dom';
 import Form from './components/Form.jsx';
 import Result from './components/Result.jsx';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Form />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Form />} />
+      <Route path="/result" element={<Result />} />
+    </Routes>
   );
 }
 
