2668325da1c7996e1385eb727930f3f8b3f9aba0
Joon Kim <joonkim@Joons-MBP.attlocal.net>
2025-12-14 20:32:30 -0800

    switch hashrouter for gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // <-- HashRouter
 import Form from './components/Form.jsx';
 import Result from './components/Result.jsx';
 
