f4ec743c907d196ef4bd52f255a8fd447d4e9b60
Joon Kim <joonkim@Joons-MBP.attlocal.net>
2025-12-14 21:56:28 -0800

    changed base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/snaphoa-account-creation/', // <-- important!
+  base: '/snaphoa-account-creation-frontend/', // <-- important!
 });
